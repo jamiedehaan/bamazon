7aec6b491d87a961fa47d026ab017e7cc91ec4d8
JAMIE DEHAAN <jamiedehaan@JAMIEs-MacBook-Pro.local>
2018-09-06 16:21:13 -0600

    ready to submit

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Bamazon is inspired by Amazon. Bamazon uses MYSQL and Inquirer. The app takes orders from customers and depletes stock from inventory. 
 
-Screencastify Link: 
+Screencastify Link: https://drive.google.com/file/d/1HTAIs1UJOVZVYKH-HnT3cDVFgVT6IVpa/view
--- bamazon.js
+++ bamazon.js
@@ -20,11 +20,10 @@ function displayProducts() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         res.forEach(function (product) {
-            console.log("\n" + product.product_name + " (Item ID: " + product.item_id + "), $" + product.price);
+            console.log("\n" + product.product_name + " (Product ID: " + product.item_id + "), $" + product.price);
         });
         promptUser();
     });
-    connection.end();
 };
 
 function promptUser() {
@@ -32,14 +31,58 @@ function promptUser() {
         {
             message: "\n" + "What is the ID of the product you would like to purchase?",
             type: "input",
-            name: "id"
+            name: "id",
+            validate: function (value) {
+                if (isNaN(value) == false) {
+                    return true;
+                } else {
+                    return false;
+                };
+            }
         },
         {
             message: "\n" + "What is your desired quantity?",
             type: "input",
-            name: "quantity"
+            name: "quantity",
+            validate: function (value) {
+                if (isNaN(value) == false) {
+                    return true;
+                } else {
+                    return false;
+                };
+            }
         }
     ])
         .then(function (response) {
-        })
-}
+            connection.query("SELECT * FROM products WHERE item_id=" + response.id, function (err, res) {
+                if (err) throw err;
+
+                var inventory = res[0].stock_quantity;
+                var payment = res[0].price;
+
+                if (response.quantity > inventory) {
+                    console.log("\nSorry, insufficient quantity!");
+                    console.log("\n****************************");
+                    displayProducts();
+
+                } else {
+                    var quantity = inventory - response.quantity;
+                    connection.query("UPDATE products SET ? WHERE ?",
+                        [
+                            {
+                                stock_quantity: quantity
+                            },
+                            {
+                                item_id: response.id
+                            }
+                        ], function (err, res) {
+                            if (err) throw err;
+                            var cost = response.quantity * payment;
+                            console.log("\nYour total is $" + cost + "!");
+                            console.log("\n****************************");
+                            displayProducts();
+                        });
+                };
+            });
+        });
+};
